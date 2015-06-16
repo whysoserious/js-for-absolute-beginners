8ef5a9f2a8bb9480d367e1d6cc034643d46aa611
Jan <mrholograham@gmail.com>
2015-06-16 22:53:30 +0200

    Meeting #1

---
+++ meeting-1.js
@@ -0,0 +1,116 @@
+13
+
+45
+
+3.456 + 5.4567
+
+2000 // 2*10^3
+
+2e3
+
+2e3 - 2e2
+
+// + - * / %
+
+100 * (2 + 5) //205 700
+
+(100 / 3) * 3
+
+100.0001 / 3e9
+
+1e-2
+
+1e-3 //1*10^-3
+
+1e-4
+
+1e2
+
+1e3 //1*10^3
+
+314 / 100
+
+330 % 100
+
+330 / 100
+
+273 % 10
+
+"twoja stara"
+
+'też twoja stara'
+
+`adfsg`
+
+"twoja stara\nw nowej linii"
+
+"twoja stara ma na imie \"Ania\"."
+
+// To jest znak nowej linii "\n"
+
+"To jest znak nowej linii \"\\n\""
+
+// To są trzy backslashe: \\\.
+
+"To są trzy backslashe: \\\\\\."
+
+"dodawanie" + " " + "stringów"
+
+"3" - "gówno"
+
+"gówno" - 3
+
+false
+
+true
+
+3 > 2
+
+3 < 6
+
+6 != 6
+
+5 >= 4
+
+"Abacki" < "Babacki"
+
+//1. Danie
+//2. Dawid
+
+"Danie" < "Dawid"
+1 < 2
+
+//1. Danie
+//2. Danielkkk
+
+"Danie" < "Danielkkk"
+1 < 2
+
+// ! zaprzeczenie / not
+
+false
+
+!false
+
+!true
+
+// && koniunkcja / and
+
+true  && true  // true
+false && true  // false
+true  && false // false
+false && false // false
+
+// || alternatywa / or
+
+true  || true  // true
+false || true  // true
+true  || false // true
+false || false // false
+
+// kolejność operatorów: ! && ||
+
+false && !true || false
+false && false || false // !
+false || false          // &&
+false                   // ||
